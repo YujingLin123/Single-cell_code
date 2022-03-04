fa9f109b5bf7795b0b8c6b20bfa8ccfa0ade4261
Yujing Lin <67212190+linyujing199488@users.noreply.github.com>
2022-03-04 21:03:10 +0800

    Update step02-check.R

--- Chapter01_RNA-seq/step02-check.R
+++ Chapter01_RNA-seq/step02-check.R
@@ -2,3 +2,9 @@ rm(list = ls())
 options(stringsAsFactors = F)
 
 if (F) {
+  load(file = '../input.Rdata')
+  a[1:4, 1:4]
+  head(metadata)
+  ## 载入第0步准备好的表达矩阵，及细胞的一些属性（hclust分群，plate批次，检测到的基因数量）
+  # 注意 变量a是原始的counts矩阵，变量 dat是log2CPM后的表达量矩阵。
+  group_list = metadata$g
