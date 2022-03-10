1edb7344c202c5e46dd572f46c6a15282ff3681d
Yujing Lin <67212190+linyujing199488@users.noreply.github.com>
2022-03-10 14:13:25 +0800

    Update step06-cell-cycle.R

--- Chapter01_RNA-seq/step06-cell-cycle.R
+++ Chapter01_RNA-seq/step06-cell-cycle.R
@@ -14,3 +14,22 @@ library(scran)
 sce <- SingleCellExperiment(list(counts=dat)) 
 #list() 创建列表
 
+library(org.Mm.eg.db)
+mm.pairs <- readRDS(system.file("exdata","mouse_cycle_markers.rds",
+                                package = "scran"))
+ensembl <- mapIds(org.Mm.eg.db, keys = rownames(sce),
+                  keytype = "SYMBOL", column = "ENSEMBL")
+#取探针名创建一个向量
+#rownames(sce) 取行名（即实验检测到的基因）
+
+##if(F)就是将{}代码注释掉，需要单独运行
+if(F){
+  assigned <- cyclone(sce,pairs = mm.pairs, gene.names = ensembl)
+  save(assigned, file = 'cell_cycle_assigned.Rdata')
+}
+
+load(file = 'cell_cycle_assigned.Rdata')
+head(assigned$scores)
+table(assigned$phases)
+
+draw = cbind()
